e026da8cdf728e52277c34369f19cabc288d8118
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-01-27 23:26:32 +0700

    add Header component to layout and update Home page content

--- Section 4 - Routing & Page Rendering/app/page.js
+++ Section 4 - Routing & Page Rendering/app/page.js
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import Header from "@/components/header";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Header />
+      Home page
     </div>
   );
 }
