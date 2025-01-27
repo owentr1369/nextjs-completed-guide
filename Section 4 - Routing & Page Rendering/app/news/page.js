0f6f3bc04651b19da4539ff7de4d1e401ea18e95
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-01-27 23:33:31 +0700

    add news page with dynamic routing and dummy data

--- Section 4 - Routing & Page Rendering/app/news/page.js
+++ Section 4 - Routing & Page Rendering/app/news/page.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { DUMMY_NEWS } from "@/conts/dummy-data";
+import Link from "next/link";
 
 const NEwsPage = () => {
-  return <div>NEwsPage</div>;
+  return (
+    <div>
+      <ul className="list-none p-0 flex flex-wrap gap-5">
+        {DUMMY_NEWS.map((news) => (
+          <li
+            key={news.id}
+            className="flex-1 min-w-[calc(33.333%-20px)] w-[calc(33.333%-20px)] box-border border border-gray-300 p-2 rounded"
+          >
+            <Link href={`/news/${news.id}`}>
+              <img
+                src={`/assets/images/${news.image}`}
+                alt={news.title}
+                className="w-full h-auto rounded"
+              />
+              <h1 className="text-xl my-2">{news.title}</h1>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NEwsPage;
